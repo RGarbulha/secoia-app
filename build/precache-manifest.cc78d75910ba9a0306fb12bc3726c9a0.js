1f08c462fa2a9caa86ce7bca32a87fbdf01146e9
rgarbulha <rodrigo.garbulha@gmail.com>
2020-07-22 17:45:30 -0400

    heroku app

---
+++ build/precache-manifest.cc78d75910ba9a0306fb12bc3726c9a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e27f15530b02817abebe6e93b75346",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a099a010e1c9a34e94c9",
+    "url": "/static/css/main.e2343e47.chunk.css"
+  },
+  {
+    "revision": "34bc7bff224d29be7b01",
+    "url": "/static/js/2.47515f89.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.47515f89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a099a010e1c9a34e94c9",
+    "url": "/static/js/main.620e5ab6.chunk.js"
+  },
+  {
+    "revision": "8e2a681409db1caed35d",
+    "url": "/static/js/runtime-main.5fc6f413.js"
+  }
+]);
